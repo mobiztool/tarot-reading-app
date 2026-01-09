de59e190041e16d00f248f6af55637b6b5b4d676
mobiztool <mobiztool@gmail.com>
2026-01-09 14:10:33 +0700

    Push package.json and tailwind config

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -26,8 +26,31 @@ module.exports = {
         },
       },
       fontFamily: {
-        serif: ['Georgia', 'serif'],
-        sans: ['Inter', 'sans-serif'],
+        // Anuphan - main font for everything
+        sans: ['var(--font-anuphan)', 'Anuphan', 'sans-serif'],
+        // Trirong - for card headings/titles
+        serif: ['var(--font-trirong)', 'Trirong', 'serif'],
+        // Specific class for card titles
+        card: ['var(--font-trirong)', 'Trirong', 'serif'],
+      },
+      animation: {
+        blob: 'blob 7s infinite',
+      },
+      keyframes: {
+        blob: {
+          '0%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+          '33%': {
+            transform: 'translate(30px, -50px) scale(1.1)',
+          },
+          '66%': {
+            transform: 'translate(-20px, 20px) scale(0.9)',
+          },
+          '100%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+        },
       },
     },
   },
